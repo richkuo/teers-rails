0f8d23d26b3c391216db569a2c759c352801ae2e
richkuo <richard.p.kuo@gmail.com>
2013-08-15 00:58:56 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Teers::Application.config.secret_key_base = '9478c0575ecc2bb67b463e60142b45d0027f34e1c3366e25bcd0315d22d34ff8ebcc0dc27b5afdf7f77176262cb4915ac8a0261350d2aff89862fbc91e9737b7'
